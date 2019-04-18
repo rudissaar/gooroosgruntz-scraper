1453803ab1563fa258081dcba1d11da0aad8e585
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-04-18 12:22:32 +0000

    Added base structure.

---
+++ gooroogruntz_scraper_config.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class GooroogruntzScraperConfig:
+    def __init__(self):
+        pass
+
---
+++ run.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse
+from gooroogruntz_scraper import GooroogruntzScraper
+
+parser = argparse.ArgumentParser()
+args = parser.parse_args()
+
