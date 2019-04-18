f56ece45749fe693a1e3a0bfe0b1438ae43a2736
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-04-18 15:39:47 +0000

    Small argument parsing related changes.

--- gooroogruntz_scraper.py
+++ gooroogruntz_scraper.py
@@ -2,6 +2,20 @@
 # -*- coding: utf-8 -*-
 
 class GooroogruntzScraper:
+    _tasks = list()
+
     def __init__(self):
-        pass
+        print('__init__')
+
+    def add_task(self, task):
+        print(task)
+        self._tasks.append(task)
+
+    def run(self):
+        print('run')
+
+        if not self._tasks:
+            self._tasks = ['battlez', 'questz']
+
+        print(self._tasks)
 
--- run.py
+++ run.py
@@ -5,5 +5,16 @@ import argparse
 from gooroogruntz_scraper import GooroogruntzScraper
 
 parser = argparse.ArgumentParser()
+parser.add_argument('-b', '--battlez', help='crawl for battlez', action='store_true')
+parser.add_argument('-q', '--questz', help='crawl for questz', action='store_true')
 args = parser.parse_args()
+
+scraper = GooroogruntzScraper()
+
+if args.battlez:
+    scraper.add_task('battlez')
+if args.questz:
+    scraper.add_task('questz')
+
+scraper.run()
 
