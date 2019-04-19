7f8b68caa29d675775b64a56c77cb1406229a8cb
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-04-19 10:55:07 +0000

    Base development.

--- gooroogruntz_scraper.py
+++ gooroogruntz_scraper.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
+
+from gooroogruntz_scraper_config import GooroogruntzScraperConfig
+
 class GooroogruntzScraper:
     _tasks = list()
 
     def __init__(self):
-        print('__init__')
+        self._container = os.path.dirname(os.path.realpath(__file__))
+        self._config = GooroogruntzScraperConfig()
 
     def add_task(self, task):
-        print(task)
         self._tasks.append(task)
 
     def run(self):
-        print('run')
-
         if not self._tasks:
             self._tasks = ['battlez', 'questz']
-
-        print(self._tasks)
-
--- gooroogruntz_scraper_config.py
+++ gooroogruntz_scraper_config.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import json
+import os
+
+from shutil import copyfile
+
 class GooroogruntzScraperConfig:
     def __init__(self):
-        pass
+        self._container = os.path.dirname(os.path.realpath(__file__))
 
+        if not self._container.endswith('/'):
+            self._container += '/'
+
+        self._config_path = self._container + 'config.json'
+
+        if not os.path.isfile(self._config_path):
+            copyfile(self._container + 'config.json.sample', self._config_path)
+
+        with open(self._config_path, 'r') as file_handle:
+            data = json.load(file_handle)
+
+        print(data)
