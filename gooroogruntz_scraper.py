b6e3dafd6b56780a4cc35c3afc09b6bda1e8a4cf
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-04-23 13:07:41 +0000

    scrape_battlez method development.

--- gooroogruntz_scraper.py
+++ gooroogruntz_scraper.py
@@ -2,6 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import os
+import re
+
+from urllib.request import urlopen, urlretrieve
+from bs4 import BeautifulSoup
+from urllib.error import HTTPError
+from urllib.error import URLError
+from urllib.parse import urlparse
 
 from gooroogruntz_scraper_config import GooroogruntzScraperConfig
 
@@ -24,9 +31,34 @@ class GooroogruntzScraper:
 
         if 'questz' in self._tasks:
             self.scrape_questz()
-            
+
     def scrape_battlez(self):
-        print(self._config.battlez_url)
+        urls = list()        
+
+        try:
+            html = urlopen(self._config.battlez_url)
+            soup = BeautifulSoup(html.read(), 'html.parser')
+        except HTTPError as err:
+            print(err)
+            return
+        except URLError as err:
+            print(err)
+            return
+
+        urls.append(self._config.battlez_url)
+
+        pagination = soup.find('ul', {'class': ['ui-pagination']})
+        pages = pagination.findChildren('li')
+
+        for page in pages:
+            try:
+                if 'next' in page['class']:
+                    link = page.findChild('a', href=True)
+
+                    if link:
+                        print(link['href'])
+            except KeyError:
+                continue
 
     def scrape_questz(self):
         print(self._config.questz_url)
