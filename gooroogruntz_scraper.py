a4749b9d899a5fde794407f16deaa73f52b94142
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-05-06 00:26:50 +0100

    General improvements.

--- gooroogruntz_scraper.py
+++ gooroogruntz_scraper.py
@@ -32,7 +32,12 @@ class GooroogruntzScraper:
             os.mkdir(self._container + '/loot', 0o700)
 
     def add_task(self, task):
-        self._tasks.append(task)
+        if task not in self._tasks:
+            self._tasks.append(task)
+
+    def remove_task(self, task):
+        if task in self._tasks:
+            self._tasks.remove(task)
 
     def run(self):
         if not self._tasks:
@@ -43,7 +48,7 @@ class GooroogruntzScraper:
             self.spider_battlez()
             self.scrape_battlez()
             self.package_battlez()
-            del self._tasks[0]
+            self.remove_task('battlez')
 
         if 'questz' in self._tasks:
             self.scrape_questz()
@@ -88,17 +93,26 @@ class GooroogruntzScraper:
                     self.download_file(link)
 
     def package_battlez(self):
-        zip = zipfile.ZipFile(self._container + '/loot/gruntz-' + self._tasks[0] + '.zip', 'w', zipfile.ZIP_DEFLATED)
+        zip_path = self._container + '/loot/gruntz-' + self.current_task + '.zip'
+        amount = 0
 
-        for root, _, files in os.walk(self._container + '/tmp/' + self._tasks[0]):
+        zip = zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED)
+
+        for root, _, files in os.walk(self._container + '/tmp/' + self.current_task):
             for file in files:
                 zip.write(os.path.join(root, file), file)
+                amount += 1
+
+        if amount:
+            os.rename(zip_path, self._container + '/loot/gruntz-' + self.current_task + '-' + str(amount) + '.zip')
+        else:
+            os.remove(zip_path)
 
     def scrape_questz(self):
         print(self._config.questz_urls)
 
     def download_file(self, url):
-        destination_dir = self._container + '/tmp/' + self._tasks[0]
+        destination_dir = self._container + '/tmp/' + self.current_task
 
         if not os.path.exists(destination_dir):
             os.mkdir(destination_dir, 0o700)
@@ -106,6 +120,12 @@ class GooroogruntzScraper:
         parts = urlparse(url)
         destination_name = destination_dir + '/' + os.path.basename(parts.path)
         urlretrieve(url, destination_name)
+
+    @property
+    def current_task(self):
+        if self._tasks:
+            return self._tasks[0]
+        return None
 
     @staticmethod
     def get_domain(url):
